55789d087d07abb51a574f8f7777e1de742919fe
zehraakarslan <zehraakarslan02@gmail.com>
2025-03-02 17:07:56 +0300

    Odev1 yapildi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,7 +16,7 @@ po.markaVitesyaz("porche", "otomatik");
 
 /*
  Soyut Sınıf (Abstract Class)
-Eyl 08, 2013
+
 Nesne tabanlı programlamada sınıf hiyerarşisi oluşturulurken, bazen en tepede bulunan sınıf türünden nesneler programcılar için anlamlı olmayabilir. Hiyerarşinin en tepesinde bulunan sınıfın kendisinden türetilecek olan alt sınıflar için ortak bir arayüz (interface) görevi yapması istenebilir. Bunun için çözüm olarak oluşturulan metotlara ve sınıflara soyut metot (abstract method) ya da soyut sınıf (abstract class) denir. Soyut sınıflar büyük projelerde kullanılırlar ve kalıtım özelliğini kullanarak kod tekrarını azaltırlar. Soyut sınıflar diğer sınıflara taban olmak için kullanılırlar. Nesne türetemezler. Önlerine “abstract” sözcüğü yazılarak soyutlaştırılırlar. Önlerine "virtual" yazılmaz çünkü “abstract” sözcüğü uygulanan tüm sınıf ve metotlar zaten sanaldır.
 
 Kullanırken dikkat edilmesi gerekenler:
